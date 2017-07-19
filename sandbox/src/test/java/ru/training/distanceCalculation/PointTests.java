e0122cc53514c337ae66d212beac4fc89a9e613b
LAPTOP-1K09BK3F\ws03 <julia.cherevko@sotsos.com>
2017-07-19 19:17:30 +0300

    PointTests fixed

--- sandbox/src/test/java/ru/training/distanceCalculation/PointTests.java
+++ sandbox/src/test/java/ru/training/distanceCalculation/PointTests.java
@@ -11,29 +11,23 @@ public class PointTests {
 
     @Test(priority = 0)
     public void DistCalc1() {
-        Point p = new Point(0, 0);
-        Point fir = new Point(2, 4);
-        Point sec = new Point(6, 9);
-
-        Assert.assertEquals(p.distance(fir, sec), 3.0);
+        Point p1 = new Point(6,7);
+        Point p2 = new Point(12,9);
+        Assert.assertEquals(p1.distance(p2), 2.8284271247461903);
     }
 
     @Test(priority = 1)
     public void DistCalc2() {
-        Point p = new Point(0, 0);
-        Point fir = new Point(7, 14);
-        Point sec = new Point(12, 17);
-
-        Assert.assertEquals(p.distance(fir, sec), 2.8284271247461903);
+        Point p1 = new Point(43,7);
+        Point p2 = new Point(67,11);
+        Assert.assertEquals(p1.distance(p2), 5.291502622129181);
     }
 
     @Test(priority = 2)
     public void DistCalc3() {
-        Point p = new Point(0, 0);
-        Point fir = new Point(17, 14);
-        Point sec = new Point(23, 40);
-
-        Assert.assertEquals(p.distance(fir, sec), 5.656854249492381);
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(12,9);
+        Assert.assertEquals(p1.distance(p2), 4.358898943540674);
 
     }
 }
